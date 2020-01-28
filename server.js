ccc31fd8d0c9da03df14d8141a39d43d381e9e57
Kurodax <l.kuroda@hotmail.com>
2020-01-28 18:00:46 -0300

    algoritmo

---
+++ server.js
@@ -0,0 +1,15 @@
+const arraynumber = [1, 6, 3, 5, 4, 2, 7];
+const guarda = [];
+
+for (let index = 0; index < arraynumber.length; index++) {
+  const element = arraynumber[index];
+
+  if (arraynumber [0] )
+  
+
+
+}
+
+
+if (arraynumber[0]< arraynumber[1])
+  if (arraynumber[1]< arraynumber[2])
